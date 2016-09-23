88940112772a76cdb7312403c01737cc6983036b
Andreas Plado <andreasplado@gmail.com>
2016-09-23 18:38:10 +0000

    Changed settings to run successfully

--- noortemaja/settings.py
+++ noortemaja/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&o@%of$5^55t6ov9qb4p)jt)pzf8p8&p=q4ll*jj1s@^gflz3!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['andreasplado.pythonanywhere.com']
 
 
 # Application definition
