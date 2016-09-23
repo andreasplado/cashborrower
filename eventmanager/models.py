60722044ec92d1981e1017e0b04848f8c7406a37
andreas plado <andreasplado@gmail.com>
2016-09-23 20:39:06 +0300

    Remover app with unusual behavior

--- eventmanager/models.py
+++ eventmanager/models.py
@@ -3,3 +3,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Event(models.Model):
+    eventName = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    place = models.CharField(max_length=100)
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    def __str__(self):
+        return self.eventName
+
