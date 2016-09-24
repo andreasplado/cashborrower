5bd6e3318b81e234157bde2f8d3a87023cdf32ee
andreas plado <andreasplado@gmail.com>
2016-09-24 17:09:38 +0300

    Made first json response page called /events

--- eventmanager/views.py
+++ eventmanager/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Event
+from .serializers import EventSerializer
 
 # Create your views here.
+
+# List all events or create a new one
+# events/
+class EventList(APIView):
+    def get(self, request):
+        events =Event.objects.all()
+        serializer = EventSerializer(events, many=True)
+        return Response(serializer.data)
+    def post(self):
+        pass
--- noortemaja/urls.py
+++ noortemaja/urls.py
@@ -15,39 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
-
-####################
-## DIVIDE CLASSES ##
-####################
-
-
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'is_staff')
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-
-####################
-####################
-
-
-
-
+from rest_framework.urlpatterns import format_suffix_patterns
+from eventmanager import views
 
 
 admin.autodiscover()
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^events/', views.EventList.as_view())
 ]
