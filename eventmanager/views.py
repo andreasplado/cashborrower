2d8d391e940a43158927201a34be71e375d956fe
andreas plado <andreasplado@gmail.com>
2016-09-26 23:15:10 +0300

    Increased pagesize in pagination(views.py)

--- eventmanager/views.py
+++ eventmanager/views.py
@@ -16,7 +16,7 @@ from rest_framework.pagination import(
 # Create your views here.
 
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 3
+    page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 1000
 
