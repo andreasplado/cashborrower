32d3eb22b55545bcaf7f0660155c2b09abeb5310
andreas plado <andreasplado@gmail.com>
2016-10-09 17:10:26 +0300

    Created fields that user don't need to update

--- eventmanager/models.py
+++ eventmanager/models.py
@@ -10,8 +10,8 @@ class Loan(models.Model):
     lender = models.CharField(max_length=100)
     borrower = models.CharField(max_length=100)
     amount = models.CharField(max_length=9)
-    notes = models.CharField(max_length=255)
-    deadline = models.DateTimeField()
+    notes = models.CharField(max_length=255,blank=True)
+    deadline = models.DateTimeField(blank=True, null=True)
     creditcount = models.IntegerField(default=0)
     discreditcount = models.IntegerField(default=0)
     
