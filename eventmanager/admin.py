a2a2d585d2f6bc8e57e7936a1d3a184b58c17fe4
andreas plado <andreasplado@gmail.com>
2016-10-10 11:08:57 +0300

    Changed Logs to Log model in admin panel

--- eventmanager/admin.py
+++ eventmanager/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Loan, LoanCredit, Comment, CommentLike, Log
 # Register your models here.
 
-admin.site.register(Logs)
+admin.site.register(Log)
 admin.site.register(CommentLike)
 admin.site.register(Comment)
 admin.site.register(LoanCredit)
