48cc1ee63edcce253a10223da12963df2d8b574d
andreas plado <andreasplado@gmail.com>
2016-10-10 11:05:49 +0300

    Registred Logs model in admin panel

--- eventmanager/admin.py
+++ eventmanager/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Loan, LoanCredit, Comment, CommentLike
+from .models import Loan, LoanCredit, Comment, CommentLike, Log
 # Register your models here.
 
-admin.site.register(Loan)
-admin.site.register(LoanCredit)
-admin.site.register(Comment)
+admin.site.register(Logs)
 admin.site.register(CommentLike)
+admin.site.register(Comment)
+admin.site.register(LoanCredit)
+admin.site.register(Loan)
+
+
+
