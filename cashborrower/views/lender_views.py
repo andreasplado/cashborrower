1cb11b92c1938ab797ea59028fea9542b247febf
andreas plado <andreasplado@gmail.com>
2016-11-08 14:57:04 +0200

    Fixed LenderByEmailAPIView

--- cashborrower/views/lender_views.py
+++ cashborrower/views/lender_views.py
@@ -43,7 +43,7 @@ class LenderDetailAPIView(generics.RetrieveAPIView):
         id = self.kwargs['id']
         return Lender.objects.filter(id=id).order_by('-id')
 
-class LenderByEmailAPIView(generics.RetrieveAPIView):
+class LenderByEmailAPIView(generics.ListCreateAPIView):
     serializer_class = lender_serializers.LenderSerializer
     pagination_class = StandardResultsSetPagination
 
