9b71f88da64253b8fe860586dd5c0cd1f7cf9c38
andreas plado <andreasplado@gmail.com>
2016-11-11 17:33:08 +0200

    Added lender loan like and dislike count

--- cashborrower/serializers/lenderloan_serializers.py
+++ cashborrower/serializers/lenderloan_serializers.py
@@ -5,6 +5,14 @@ from ..models import  Loan
 
 
 class LenderLoanSerializer(serializers.ModelSerializer):
+    loanLikeCount = serializers.IntegerField(
+        source='loanlike_set.count',
+        read_only=True
+    )
+    loanDislikeCount = serializers.IntegerField(
+        source='loandislike_set.count',
+        read_only=True
+    )
     class Meta:
         model = Loan
         fields =('id','lender','borrower', 'amount','notes','deadline', 'isLoanPrivate')
