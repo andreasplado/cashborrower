f60851a10e355d5a0458834b3d44525ce78b97b0
andreas plado <andreasplado@gmail.com>
2016-11-16 16:56:17 +0200

    UserAddAPIView changes

--- cashborrower/views/user_views.py
+++ cashborrower/views/user_views.py
@@ -1,5 +1,3 @@
-from rest_framework import viewsets
-
 from ..models import User
 from rest_framework.pagination import(
     PageNumberPagination
@@ -28,15 +26,10 @@ class UserDetailAPIView(generics.RetrieveAPIView):
     serializer_class = user_serializers.UserSerializer
     lookup_field = 'id'
 
-class UserAddAPIView(viewsets.ModelViewSet):
+class UserAddAPIView(generics.CreateAPIView):
     queryset = User.objects.order_by('-id')
     serializer_class = user_serializers.UserSerializer
     pagination_class = StandardResultsSetPagination
-    def get_object(self):
-        if self.request.method == 'PUT':
-            obj, created = User.objects.get_or_create(gmail=self.kwargs.get('gmail'))
-        if self.request.method == 'GET':
-            obj, created = User.objects.get(gmail=self.kwargs.get('gmail'))
 
 
 class UserUpdateAPIView(generics.UpdateAPIView):
