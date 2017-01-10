b683fa3abf2fd4d8edc103dc49776deb520d6a1d
andreas plado <andreasplado@gmail.com>
2017-01-10 21:14:58 +0200

    Minor changes in settings.py

--- cashborrower/settings.py
+++ cashborrower/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cashborrowerAPI',
-    'cashborrower',
     'rest_framework.authtoken',
     'rest_framework',
 ]
