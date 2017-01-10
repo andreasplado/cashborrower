6f17f828afbd3361375033d3e55329cbdf675eeb
andreas plado <andreasplado@gmail.com>
2017-01-10 21:26:56 +0200

    Changed settings

--- cashborrower/settings.py
+++ cashborrower/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'domain.urls'
+ROOT_URLCONF = 'cashborrower.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'domain.wsgi.application'
+WSGI_APPLICATION = 'cashborrower.wsgi.application'
 
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
