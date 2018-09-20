7f44c18dd7557ae587c3901a159d5250b116b9a2
Andreas Plado <andreasplado@gmail.com>
2018-09-20 23:35:48 +0300

    Updated static files

--- cashborrower/settings.py
+++ cashborrower/settings.py
@@ -145,9 +145,9 @@ STATIC_URL =  '/static/'
 
 
 #change to server
-STATICFILES_DIRS = (
-    (os.path.join(BASE_DIR, "static"),)
-)
+# STATICFILES_DIRS = (
+#     (os.path.join(BASE_DIR, "static"),)
+# )
 
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
