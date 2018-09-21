d6df84a94f0eb35eadcd6051f269f3f57c4ba0db
Andreas Plado <andreasplado@gmail.com>
2018-09-21 13:27:05 +0300

    Refactoring

--- static/js/controllers/index_controller.js
+++ static/js/controllers/index_controller.js
@@ -80,7 +80,7 @@ $(document).ready(function(){
             toastr["error"]("Your passwords do not match!");
         }
         if(registerUsernameTxt!= "" && registerPasswordTxt !=""){
-            data =  JSON.stringify({username : usernameTxt, password : passwordTxt })
+            data =  JSON.stringify({username : registerUsernameTxt, password : registerPasswordTxt })
             $.ajax({
                 type: "POST",
                 url: "/api-token-auth/",
