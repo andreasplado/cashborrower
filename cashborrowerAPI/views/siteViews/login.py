d7368a3376b693d16054ea9baa9f751384f93351
Andreas Plado <andreasplado@gmail.com>
2018-09-28 01:54:54 +0300

    Refactoring#

--- cashborrowerAPI/views/siteViews/login.py
+++ cashborrowerAPI/views/siteViews/login.py
@@ -1,7 +1,4 @@
 from django.template.context_processors import csrf
-from pip import logger
-
-from cashborrowerAPI.views.siteViews.register import user_exists, create_user
 from django.shortcuts import redirect, render_to_response
 from django.contrib.auth import authenticate, login
 
@@ -14,9 +11,6 @@ def view_login(request):
 def view_auth_and_login(request, onsuccess='/', onfail='/login/'):
     username = request.POST.get("username", None)
     password = request.POST.get("password", None)
-    #print(' username: ' + username +' password: ' + password + 'request: '+ request)
-    print(request.POST.get('username', ''))
-    print(request.POST.get('password', ''))
     user = authenticate(username=username, password=password)
 
     if user is not None:
@@ -24,16 +18,5 @@ def view_auth_and_login(request, onsuccess='/', onfail='/login/'):
         return redirect(onsuccess)
     else:
         return redirect(onfail)
-
-def sign_up(request, onsuccess='/', onfail='/login/'):
-    username = request.POST.get("username", None);
-    email = request.POST.get("email", None)
-    password = request.POST.get("password", None);
-
-    if not user_exists(email):
-        user = create_user(username=username, email=email, password=password)
-        return view_auth_and_login(request)
-    else:
-        return redirect("/")
 
 
