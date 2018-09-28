303faa3c2230c714fdf7e960389a551fec55f73e
Andreas Plado <andreasplado@gmail.com>
2018-09-28 19:03:40 +0300

    Udpated register

--- cashborrowerAPI/views/siteViews/sign_up.py
+++ cashborrowerAPI/views/siteViews/sign_up.py
@@ -1,16 +1,18 @@
 from django.shortcuts import redirect
+from django.template.context_processors import csrf
 
 from cashborrowerAPI.views.siteViews.login import view_auth_and_login
 from cashborrowerAPI.views.siteViews.register import user_exists, create_user
 
 
 def sign_up(request, onsuccess='/', onfail='/login/'):
-    username = request.POST.get("username", None);
+    username = request.POST.get("username", None)
     email = request.POST.get("email", None)
-    password = request.POST.get("password", None);
-
-    if not user_exists(email):
+    password = request.POST.get("password", None)
+    c = {}
+    c.update(csrf(request))
+    if not user_exists(username):
         create_user(username=username, email=email, password=password)
-        return redirect("/")
+        return redirect("/", c)
     else:
-        return view_auth_and_login(request)
+        return redirect("/login/", c)
