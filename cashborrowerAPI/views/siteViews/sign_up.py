dd740f80c9d6ecc3821b858c9ae431c435f78464
Andreas Plado <andreasplado@gmail.com>
2018-09-28 18:35:16 +0300

    Udpated register

--- cashborrowerAPI/views/siteViews/sign_up.py
+++ cashborrowerAPI/views/siteViews/sign_up.py
@@ -8,6 +8,8 @@ def sign_up(request, onsuccess='/', onfail='/login/'):
     username = request.POST.get("username", None);
     email = request.POST.get("email", None)
     password = request.POST.get("password", None);
-
-    create_user(username=username, email=email, password=password)
-    return redirect("/")
+    if not user_exists(username):
+        create_user(username=username, email=email, password=password)
+        return redirect("/")
+    else:
+        return redirect(onfail)
